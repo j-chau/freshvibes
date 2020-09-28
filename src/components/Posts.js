8f1986f642f72f989c0ded3fdb9bc1c41933dbe8
Joey <joeymchau@gmail.com>
2020-09-28 14:23:50 -0400

    create template for iframe

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -8,8 +8,21 @@ const Post = ({ header, description, songList }) => {
             <p>{description}</p>
             <ul>
                 {songList.map((el, index) => {
+                    const songUrl = el.embedSong;
+                    const albumIdStart = songUrl.indexOf("album") + "album".length + 1;
+                    const albumIdEnd = songUrl.indexOf("/", albumIdStart);
+                    const albumId = songUrl.slice(albumIdStart, albumIdEnd);
+                    // console.log(albumId);
+
+                    const urlString = "https://bandcamp.com/EmbeddedPlayer/album=" + albumId + "/size=small/bgcol=f5f1ea/linkcol=0687f5/transparent=true/";
+                    const artistCredit = `${el.title} by ${el.artist}`;
+
                     return (
-                        <li className="songInfo" key={index}><a href={el.url}>{el.title}</a></li>
+                        <li className="songInfo" key={index}>
+                            <iframe style={{ border: "0", width: "100%", height: "42px" }} src={urlString} title={artistCredit} seamless>
+                                <a href={el.embedSong}>{artistCredit}</a>
+                            </iframe>
+                        </li>
                     )
                 })}
             </ul>
