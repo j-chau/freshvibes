6802193ed377854800f5cd6def40bdba90eba662
Joey <joeymchau@gmail.com>
2020-09-30 15:19:30 -0400

    Updates

---
+++ precache-manifest.debde7b2a8e833baf50f3a88fdf05c7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f0b2e99b6cd67c33259782e4a7e1cc",
+    "url": "/joeyChauProjectFive/index.html"
+  },
+  {
+    "revision": "bd13c02c6184c6711b35",
+    "url": "/joeyChauProjectFive/static/css/main.e7dc27b8.chunk.css"
+  },
+  {
+    "revision": "ba0c7690bdef8eb44031",
+    "url": "/joeyChauProjectFive/static/js/2.ad601185.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/joeyChauProjectFive/static/js/2.ad601185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd13c02c6184c6711b35",
+    "url": "/joeyChauProjectFive/static/js/main.01377f90.chunk.js"
+  },
+  {
+    "revision": "8cee1024a95cf850ed07",
+    "url": "/joeyChauProjectFive/static/js/runtime-main.4cd48c2a.js"
+  }
+]);
